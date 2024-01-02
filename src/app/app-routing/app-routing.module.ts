5d43d5f50c61c807a70bf9dda131d2378ae7f183
lezak <lezakis@gmail.com>
2024-01-02 17:23:45 +0200

    nav-bar completed

---
+++ src/app/app-routing/app-routing.module.ts
@@ -0,0 +1,32 @@
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from "@angular/router";
+import {ProductListComponent} from "../product-list/product-list.component";
+import {ProductComponent} from "../product/product.component";
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'products' ,
+    pathMatch: 'full'
+  },
+  {
+    path: 'products',
+    component: ProductListComponent
+  },
+  {
+    path: 'favorites',
+    component: ProductListComponent
+  },
+  {
+    path: 'products/:id',
+    component: ProductComponent
+  }
+]
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {NavBarComponent} from "./nav-bar/nav-bar.component";
+import {ProductComponent} from "./product/product.component";
+import {ProductListComponent} from "./product-list/product-list.component";
+import {AppComponent} from "./app.component";
+import {AppRoutingModule} from "./app-routing/app-routing.module";
+import {BrowserModule} from "@angular/platform-browser";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent ,
+    NavBarComponent ,
+    ProductComponent ,
+    ProductListComponent,
+
+  ],
+  imports: [
+    CommonModule,
+    AppRoutingModule,
+    BrowserModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    BrowserAnimationsModule,
+
+  ],
+  bootstrap:[AppComponent]
+})
+export class AppModule { }
