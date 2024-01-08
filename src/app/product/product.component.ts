27b3851290cd4023911a3e4c4cb623f617096763
lezak <lezakis@gmail.com>
2024-01-08 21:27:50 +0200

    star reform

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,45 +1,43 @@
 import {Component, OnInit} from '@angular/core';
 import {ProductService} from "../_services/product.service";
-import {FavoriteService} from "../_services/favorite.service";
 import {ActivatedRoute} from "@angular/router";
 import {Product} from "../_models/product";
 
 @Component({
-  selector: 'app-product',
-  templateUrl: './product.component.html',
-  styleUrl: './product.component.css'
+    selector: 'app-product',
+    templateUrl: './product.component.html',
+    styleUrl: './product.component.css'
 })
 export class ProductComponent implements OnInit {
 
-  product!: Product  ;
-  selectedIndex : number = 0 ;
+    product!: Product;
+    selectedIndex: number = 0;
 
-  constructor(private productService: ProductService,
-              private activatedRoute: ActivatedRoute) {
+    constructor(private productService: ProductService,
+                private activatedRoute: ActivatedRoute) {
 
-  }
+    }
 
-  ngOnInit() {
-    let produvtId = +this.activatedRoute.snapshot.params['id'];
-    this.productService.getProduct(produvtId).subscribe(
-        resp => {
-          this.product=resp;
+    ngOnInit() {
+        let produvtId = +this.activatedRoute.snapshot.params['id'];
+        this.productService.getProduct(produvtId).subscribe(
+            resp => {
+                this.product = resp;
 
-        },
-        error => {
-          console.log(error)
-       }
-    );
+            },
+            error => {
+                console.log(error)
+            }
+        );
 
 
-  }
+    }
 
-  updateIndex(imageIndex:number){
+    updateIndex(imageIndex: number) {
 
-    this.selectedIndex = imageIndex ;
+        this.selectedIndex = imageIndex;
 
-  }
-
+    }
 
 
 }
